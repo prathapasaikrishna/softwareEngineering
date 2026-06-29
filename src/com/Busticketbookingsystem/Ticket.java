64225c47ad97e370fa6559a56e6474c486b3e05f
sasi <sasikumarnelavala>
2026-06-29 08:50:12 +0530

    Added Ticket Class

--- src/com/Busticketbookingsystem/Ticket.java
+++ src/com/Busticketbookingsystem/Ticket.java
@@ -10,11 +10,7 @@ public class Ticket {
     private Bus bus;
     private int numberOfTickets;
     private double fare;
-<<<<<<< HEAD
-    private List<Integer> bookedSeats; // stores 0-based seat indices
-=======
     private List<Integer> bookedSeats;
->>>>>>> 0e424c2 (Added Ticket class)
 
     public Ticket(int ticketId, Bus bus, int numberOfTickets,
                   double fare, int customerId) {
@@ -26,11 +22,8 @@ public class Ticket {
         this.customerId = customerId;
         this.bookedSeats = new ArrayList<>();
     }
-<<<<<<< HEAD
 
-    // Called after each successful bookSeat()
-=======
->>>>>>> 0e424c2 (Added Ticket class)
+
     public void addBookedSeat(int seatNumber) {
         bookedSeats.add(seatNumber);
     }
